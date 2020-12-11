a943d9b7c3d66fb36fe97a709b12b635353eee5b
Tan Vu <lap>
2020-12-11 14:44:00 +0700

    Update cad.js

--- cad.js
+++ cad.js
@@ -1,18 +1,23 @@
 //import * as THREE from 'three';
 //import ViewCubeControls from './ViewCubeControls';
 import * as THREE from './threejs/build/three.module.js';
+import { OrbitControls } from './threejs/build/jsm/controls/OrbitControls.js';
 import ViewCubeControls from './ViewCubeControls.js';
 
 // <Scene>
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+let controls;
 document.querySelector('#app').appendChild(renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.set(0, 20, 0);
 scene.add(new THREE.GridHelper(100, 10, 0xfffffff));
 scene.add(new THREE.AxesHelper(30));
+
+controls = new OrbitControls( camera, renderer.domElement );
+
 // </Scene>
 
 // <CubeScene>
@@ -34,6 +39,8 @@ viewCube.addEventListener('angle-change', ({ quaternion }) => {
 
 function update() {
   requestAnimationFrame(update);
+  // required if controls.enableDamping or controls.autoRotate are set to true
+  controls.update();
   viewCube.update();
   renderer.render(scene, camera);
   cubeRenderer.render(cubeScene, cubeCamera);
